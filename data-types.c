a64a10877a831c7ed3320a27a4710dff30729a77
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-14 10:13:01 -0300

    incluindo data types

---
+++ data-types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int idade = 28; // Inteiro
+    float altura = 1.75;   // Numero decimal
+    char inicial = "J" // Caractere
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Inicial: %c\n", inicial);
+
+}
